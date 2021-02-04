50efc9185c9f0bee59c727072ce74a789daabf27
Alex Groome <agroome45@verizon.net>
2021-02-04 14:17:09 -0500

    Working on BFS

--- maze_on_fire/maze_on_fire.py
+++ maze_on_fire/maze_on_fire.py
@@ -1,3 +1,4 @@
+import queue
 import random
 from random import randint
 
@@ -57,7 +58,45 @@ def DFS(matrix, start_location, end_location, dim): # Uses DFS to determine if o
                     if(closed.count([i,j-1]) == 0 and fringe.count([i,j-1]) == 0):
                         fringe.append([i,j-1])
             closed.append(current)  #puts current state in closed after generating valid children
-    return False 
+    return False
+
+def BFS(matrix, start_location, end_location, dim): # Uses DFS to determine if one state is reachable from another
+    fringe = queue.Queue()
+    closed = []
+    inFringe = []
+    fringe.put(start_location)
+    while(fringe.empty() == False):
+        print(fringe.queue, "Fringe") #prints fringe (NOT NEEDED)
+        current = fringe.get()  #Pops state from fringe and makes current
+        print(current, "current") #prints current state (NOT NEEDED)
+        if(current == end_location):
+            return True
+        else:
+            i = current[0]
+            j = current[1]
+            if((i + 1) >= 0 and (i + 1) < dim ):   # Checks if the following state is in the maze range   
+                if(matrix[i+1][j] == "O" or matrix[i+1][j] == "G"):  #checks is the following state is a open or goal state
+                    if(closed.count([i+1,j]) == 0 and inFringe.count([i+1,j]) == 0 ): #checks if the following state isn't already closed or in the fringe
+                        fringe.put([i+1,j])
+                        inFringe.append([i+1,j])
+            if((i - 1) >=0 and (i - 1) < dim):
+                if(matrix[i-1][j] == "O" or matrix[i-1][j] == "G" ):
+                    if(closed.count([i-1,j]) == 0 and inFringe.count([i-1,j]) == 0 ):
+                        fringe.put([i-1,j])
+                        inFringe.append([i-1,j])
+            if((j + 1) >=0 and (j + 1) < dim):
+                if(matrix[i][j+1] == "O" or matrix[i][j+1] == "G"):
+                    if(closed.count([i,j+1]) == 0 and inFringe.count([i,j+1]) == 0  ):
+                        fringe.put([i,j+1])
+                        inFringe.append([i,j+1]) 
+            if((j - 1) >= 0 and (j - 1) < dim):
+                if(matrix[i][j-1] == "O" or matrix[i][j-1] == "G"):
+                    if(closed.count([i,j-1]) == 0 and inFringe.count([i,j-1]) == 0):
+                        fringe.put([i,j-1])
+                        inFringe.append([i,j-1])
+            closed.append(current)  #puts current state in closed after generating valid children
+    return False  
+
      
             
 dim = int(input("Enter Size dim: "))
@@ -68,3 +107,6 @@ maze = create_maze(dim, p)
 print_maze(maze, dim)
 print("DFS which determines if G can be reached from S")
 print(DFS(maze, [0,0], [dim-1,dim-1], dim))
+print()
+print("BFS which determines if G can be reached from S")
+print(BFS(maze, [0,0], [dim-1,dim-1], dim))
