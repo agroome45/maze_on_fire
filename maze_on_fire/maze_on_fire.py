827fc942d457929e9096f25484f89956dcedabe7
Ricky-Lopez <rllopez718@gmail.com>
2021-02-06 14:55:57 -0500

    Added A* algorithm, still need to add traceback feature, and reaffirm that it is working as intended.

--- maze_on_fire/maze_on_fire.py
+++ maze_on_fire/maze_on_fire.py
@@ -1,5 +1,6 @@
 import queue
 import random
+import math
 from random import randint
 
 def create_maze(dim,p): # creates a maze given dem and p
@@ -33,7 +34,7 @@ def DFS(matrix, start_location, end_location, dim): # Uses DFS to determine if o
     closed = []
     fringe.append(start_location)
     while(len(fringe) != 0):
-        print(fringe, "fringe") #prints fringe (NOT NEEDED)
+        print(fringe, "Fringe") #prints fringe (NOT NEEDED)
         current = fringe.pop()  #Pops state from fringe and makes current
         print(current, "current") #prints current state (NOT NEEDED)
         if(current == end_location):
@@ -97,16 +98,66 @@ def BFS(matrix, start_location, end_location, dim): # Uses DFS to determine if o
             closed.append(current)  #puts current state in closed after generating valid children
     return False  
 
-     
-            
-dim = int(input("Enter Size dim: "))
-p = float(input("Enter Probability (0 < p < 1) p:"))
+def Astar(matrix, start_location, end_location, dim):
+    fringe = []
+    closed = []
+    fringe.append(start_location)
+    distance = 0
+    while( fringe ):
+        print(fringe, "Fringe")
+        current = fringe.pop(0)
+        print(current, "current")
+        x = current[0]
+        y = current[1]
+        if matrix[x][y] == 'G' :
+            return True  #TODO return path instead of boolean
+        else :
+            if ((x+1) >= 0 and (x+1) < dim) :
+                if(matrix[x+1][y] == "O" or matrix[x+1][y] == "G"):
+                    if(closed.count([x+1,y]) == 0 and fringe.count([x+1,y]) == 0) :
+                        fringe = sortedInsert(fringe, [x+1, y, distance + euclideanDistance([x+1, y] , end_location)])
+            if ((x-1) >= 0 and (x-1) < dim) :
+                if(matrix[x-1][y] == "O" or matrix[x-1][y] == "G"):
+                    if(closed.count([x-1,y]) == 0 and fringe.count([x-1,y]) == 0) :
+                        fringe = sortedInsert(fringe, [x-1, y, distance + euclideanDistance([x-1, y] , end_location)])                        
+            if ((y+1) >= 0 and (y+1) < dim) :
+                if(matrix[x][y+1] == "O" or matrix[x][y+1] == "G"):
+                    if(closed.count([x,y+1]) == 0 and fringe.count([x,y+1]) == 0) :
+                        fringe = sortedInsert(fringe, [x, y+1, distance + euclideanDistance([x, y+1] , end_location)])
+            if ((y-1) >= 0 and (y-1) < dim) :
+                if(matrix[x][y-1] == "O" or matrix[x][y-1] == "G"):
+                    if(closed.count([x,y-1]) == 0 and fringe.count([x,y-1]) == 0) :
+                        fringe = sortedInsert(fringe, [x, y-1, distance + euclideanDistance([x, y-1] , end_location)])
+        distance = distance + 1
+        closed.append([current])
+    return False
+    #TODO add inFringe list to assure no duplicates are placed in the fringe.
 
-print()
-maze = create_maze(dim, p)
-print_maze(maze, dim)
-print("DFS which determines if G can be reached from S")
-print(DFS(maze, [0,0], [dim-1,dim-1], dim))
-print()
-print("BFS which determines if G can be reached from S")
-print(BFS(maze, [0,0], [dim-1,dim-1], dim))
+def sortedInsert(Alist, item) : #Helper function for Astar. 
+
+    for i in range(len(Alist)) :
+        if Alist[i][2] >= item[2] : #[2] is the distance from the start_location.
+            retList = Alist[:i] + [item] + Alist[i:] 
+            return retList
+    Alist.append(item)
+    return Alist
+
+def euclideanDistance(start_point, end_point): #Function for euclidean metric heuristic
+    horizontalDiff = math.sqrt((end_point[0] - start_point[0]) ** 2)
+    verticalDiff = math.sqrt((end_point[1] - start_point[1]) ** 2)
+    return math.sqrt((verticalDiff**2) + (horizontalDiff**2))
+
+if __name__ == "__main__" :            
+    dim = int(input("Enter Size dim: "))
+    p = float(input("Enter Probability (0 < p < 1) p:"))
+
+    print()
+    maze = create_maze(dim, p)
+    print_maze(maze, dim)
+    print("DFS which determines if G can be reached from S")
+    print(DFS(maze, [0,0], [dim-1,dim-1], dim))
+    print()
+    print("BFS which determines if G can be reached from S")
+    print(BFS(maze, [0,0], [dim-1,dim-1], dim))
+    print("Astar which determines the shortest path from S to G")
+    print(Astar(maze, [0,0], [dim-1, dim-1], dim))
